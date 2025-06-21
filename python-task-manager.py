07723a06c67357614969be318e61c57728cbfff7
mahmoudjadid <mahmoudjadid@outlook.com>
2025-06-21 13:09:56 +0300

    fixing

--- python-task-manager.py
+++ python-task-manager.py
@@ -1,5 +1,15 @@
 
+queue=[]
 
+def task(title, duration, priority):
+    title=input("enter task title : ")
+    duration=int(input("enter duration for this tank : "))
+    prior=int(input("enter the prioriy : "))
+    
+def isempty():
+     if len(queue)==0:
+         print('no tasks yet')
+    
 
 
 
